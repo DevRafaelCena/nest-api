bf4cf35c5386990b8952f967b61362f208b7a498
Rafael Cena <rafael.cena@hotmail.com>
2023-02-26 11:33:47 -0300

    dto update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Get, Param, Post, Put, Patch , Delete } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user-dto';
+import { UpdatePatchUserDto } from './dto/update-patch-user-dto';
+import { UpdatePutUserDto } from './dto/update-put-user-dto';
 
 @Controller('user')
 export class UserController {
@@ -21,13 +23,13 @@ export class UserController {
   }
 
   @Put(':id')
-  async update(@Param('id') id, @Body() body) {
+  async update(@Param('id') id, @Body() { email, name , password } : UpdatePutUserDto) {
     console.log(id);
-    return body;
+    return { email, name , password };
   }
 
   @Patch(':id')
-  async updateOne(@Param('id') id, @Body() body) {
+  async updateOne(@Param('id') id, @Body() body: UpdatePatchUserDto) {
     console.log(id);
     return body;
   }

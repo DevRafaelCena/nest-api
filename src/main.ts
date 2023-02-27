a22cea7049eae830cdbbd6d087b0cd757782bcfa
Rafael Cena <rafael.cena@hotmail.com>
2023-02-27 15:08:51 -0300

    interceptor global

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { LogInterceptor } from './user/interceptors/log.interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LogInterceptor());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
